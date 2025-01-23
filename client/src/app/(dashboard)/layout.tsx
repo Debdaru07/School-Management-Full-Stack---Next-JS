2ad094d89cce15ab06a96de7470a44000f428ae3
Debdaru <debdarudasgupta0799.com>
2025-01-24 02:29:29 +0530

    menu ui component made

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import Menu from "../../components/Menu"
 
 export default function DashboardLayout({
     children,
@@ -9,11 +10,12 @@ export default function DashboardLayout({
     return (
         <div className="h-screen flex">
           {/* LEFT */}
-          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-            <Link href='/' className="flex items-center justify-center">
+          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+            <Link href='/' className="flex items-center justify-start gap-2">
               <Image src="/logo.png" alt="logo" width={32} height={32}/>
               <span className= "hidden lg:block">Smart Scholars</span>
             </Link>
+            <Menu></Menu>
           </div>
           {/* RIGHT */}
           <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">R</div>
